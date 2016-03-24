a4b115ecda45cb20d76dd83df23cea37f0e14133
Karin Fernandez <karinfdez@yahoo.com>
2016-03-24 18:07:05 -0400

    Added validations to input fields

--- src/app/contact/contact.controller.js
+++ src/app/contact/contact.controller.js
@@ -6,17 +6,27 @@
     .controller('MessageController', MessageController);
 
   /** @ngInject */
-  function MessageController($log,API,$http) {
+  function MessageController($log,API,$http,$timeout) {
     var vm = this;
     vm.sendMessage = sendMessage;
+    vm.showMessage = false;
+    vm.showSign=false;
     vm.message={};
 
     function sendMessage(){
-      
+      vm.sendingMessage="Sending Message..."
+      vm.showSign=true;
       var data={message: {name: vm.message.name, email: vm.message.email,content: vm.message.content}};
-      $log.debug(data);
+      // $log.debug(data);
        $http.post(API+'/contact',data).success(function(){
-          // alert("Message sent");
+          vm.showSign=false;
+          // To leave inputs field in blank
+          // $timeout(function () { vm.showSign= false }, 3000); 
+          vm.message={};
+          vm.sentMessage="Thanks, your message have been sent to Karin Fernandez.";
+          vm.showMessage=true;
+          $timeout(function () { vm.showMessage= false }, 6000);  
+          
        })
        .error(function(error){
           $log.debug(error);
@@ -28,17 +38,3 @@
 
 
      
-
-
-
-//       var data={message: {name: $scope.message.name, email: $scope.message.email,content: $scope.message.content}};
-//       // console.log(data);
-//      $http.post("https://cart-project.herokuapp.com/contact",data).success(function(data){
-//         $scope.message = angular.copy(resetMessage);
-//         alert("Message sent");
-//      })
-//      .error(function(error){
-//       console.log("Couldn't send message");
-//      })
-//     }
-// }])

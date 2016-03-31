6bdbcedea555636357b11d43a3ea6ee24ab6211c
Karin Fernandez <karinfdez@yahoo.com>
2016-03-30 20:39:58 -0400

    Edited  index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// var express = require('express'),
+// app = express();
+
+// app.use(express.static(__dirname  + '/dist'));
+
+// app.get('/', function(req, res) {
+//     res.sendfile('index.html', {root: __dirname })
+// });
+// var server = app.listen(process.env.PORT || 80);
+
+
 var express = require('express');
 var app = express();
 
@@ -9,9 +20,10 @@ app.use(express.static(__dirname + '/dist'));
 // app.set('views', __dirname + '/views');
 // app.set('view engine', 'ejs');
 
-// app.get('/', function(request, response) {
-//   response.render('pages/index');
-// });
+app.get('/', function(request, response) {
+  // response.render('pages/index');
+  response.render('index');
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
